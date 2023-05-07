c27a908bf23c5f3f0dd79de83212b7984853ed6c
N0pulse <thomasabad532@gmail.com>
2023-05-06 23:02:04 -0400

    Added ram, cpu, datetime, and internet activity.

--- config.h
+++ config.h
@@ -65,5 +65,9 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+	//{ datetime, "%s",           "%F %T" },
+	{ netspeed_rx, "%sB/s ", "wlan0"      },
+	{ cpu_perc, "[CPU   %s%%]  ", NULL   },
+	{ ram_perc, "[RAM   %s%%]  ", NULL   },
+	{ datetime, "%s",       "%a %b %d %r" },
 };
